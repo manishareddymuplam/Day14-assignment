e0739ff6758f3598af5840518a7e9fe50e606a46
manishareddymuplam <manishamuplam@gmail.com>
2022-07-11 23:54:51 +0530

    added:creat stack

---
+++ cm/bridgelabz/stack/Stack.java
@@ -0,0 +1,41 @@
+package cm.bridgelabz.stack;
+
+public class Stack {
+	//public static void main(String[] args) {
+		public static void main(String[] args) {
+
+				Stack stack = new Stack();
+				System.out.println("Stack Operation Adding");
+				stack.push(70);
+				stack.display();
+				stack.push(30);
+				stack.display();
+				stack.push(56);
+				stack.display();
+				System.out.println("Stack Operation Deleting");
+				stack.display();
+				stack.pop();
+				stack.display();
+				stack.pop();
+				stack.display();
+				stack.pop();
+				stack.display();
+
+
+			}
+
+	private void pop() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	private void display() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	private void push(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+		}
